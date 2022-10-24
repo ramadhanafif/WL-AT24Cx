9812d4c52d908dd8bfb47bfa48e64a73930d01bb
Afif via g14 <ramadhanafif@gmail.com>
2022-10-24 18:20:43 +0700

    clang-formatted

--- WL_AT24CX.h
+++ WL_AT24CX.h
@@ -13,7 +13,8 @@
 
 #include "AT24CX.h"
 
-template <typename data_t> struct wl_data_t {
+template <typename data_t>
+struct wl_data_t {
     data_t data;
     uint32_t ptr;
 };
@@ -23,8 +24,9 @@ template <typename data_t> struct wl_data_t {
  *
  * @tparam data_t data type to be stored in eeprom
  */
-template <class data_t> class WL_AT24CX : public AT24CX {
-  public:
+template <class data_t>
+class WL_AT24CX : public AT24CX {
+   public:
     /**
      * @brief Construct a new WLAT24CX object
      *
@@ -35,8 +37,13 @@ template <class data_t> class WL_AT24CX : public AT24CX {
      * @param wl_en enable/disable wear leveling
      * @param eeprom_size eeprom size, in bytes
      */
-    WL_AT24CX(byte index, byte pageSize, uint32_t base_addr,
-              uint32_t num_of_data, bool wl_en, uint32_t eeprom_size = 1 << 15)
+    WL_AT24CX(
+        byte index,
+        byte pageSize,
+        uint32_t base_addr,
+        uint32_t num_of_data,
+        bool wl_en,
+        uint32_t eeprom_size = 1 << 15)
         : AT24CX(index, pageSize)
     {
         this->base_addr   = base_addr;
@@ -67,15 +74,12 @@ template <class data_t> class WL_AT24CX : public AT24CX {
             current_ptr = wl_peek(taddr).ptr;
             next_ptr    = wl_peek(taddr + 1).ptr;
 
-            // ESP_LOGI("EEPROM", "TADDR %d: current ptr = %d, next ptr %d", taddr, current_ptr, next_ptr);
-
             // IF found break in pointer array
             if ((next_ptr - current_ptr != 1) || (next_ptr == pointer_max)) {
                 taddr_current  = (taddr + 1) % num_of_data; // circular taddr
                 taddr_last     = taddr;
                 wl_ptr_current = current_ptr + 1;
-                ESP_LOGI("EEPROM", "Obtained taddr = %d, ptr %d", taddr_current,
-                         wl_ptr_current);
+                ESP_LOGI("EEPROM", "Obtained taddr = %u, ptr %u", taddr_current, wl_ptr_current);
 
                 break;
             }
@@ -92,10 +96,10 @@ template <class data_t> class WL_AT24CX : public AT24CX {
     {
         assert(wl_enable);
 
-        wl_data_t<data_t> buffer = {.data = data, .ptr = wl_ptr_current};
+        wl_data_t<data_t> buffer = { .data = data, .ptr = wl_ptr_current };
 
         uint32_t addr = taddr_to_addr(taddr_current);
-        write(addr, reinterpret_cast<byte*>(&buffer), wl_data_size);
+        write(addr, reinterpret_cast<byte *>(&buffer), wl_data_size);
 
         wl_ptr_current++;
         taddr_last    = taddr_current;
@@ -113,7 +117,7 @@ template <class data_t> class WL_AT24CX : public AT24CX {
         // enforce circular addressing on non-wl methods
         uint32_t addr = taddr_to_addr(taddr % num_of_data);
         data_t buffer = data;
-        write(addr, reinterpret_cast<byte*>(&buffer), data_size);
+        write(addr, reinterpret_cast<byte *>(&buffer), data_size);
     }
 
     /**
@@ -125,8 +129,7 @@ template <class data_t> class WL_AT24CX : public AT24CX {
     data_t read_mem(const uint32_t taddr)
     {
         data_t out;
-        read(taddr_to_addr(taddr % num_of_data), reinterpret_cast<byte*>(&out),
-             data_size);
+        read(taddr_to_addr(taddr % num_of_data), reinterpret_cast<byte *>(&out), data_size);
         return out;
     }
 
@@ -136,7 +139,10 @@ template <class data_t> class WL_AT24CX : public AT24CX {
      *
      * @return * uint32_t end address
      */
-    uint32_t get_end_addr() { return end_addr; }
+    uint32_t get_end_addr()
+    {
+        return end_addr;
+    }
 
     /**
      * @brief WIPE data from eeprom, reset to 0xFF
@@ -149,17 +155,23 @@ template <class data_t> class WL_AT24CX : public AT24CX {
         uint64_t max = -1;
         for (int i = 0; i < size; i += sizeof(uint64_t)) {
             ESP_LOGD("EEPROM", "Wiping process: %.2f", 100.0 * i / size);
-            write(i, reinterpret_cast<byte*>(&max), sizeof(uint64_t));
+            write(i, reinterpret_cast<byte *>(&max), sizeof(uint64_t));
         }
     }
-    void wipe() { wipe(eeprom_size); }
+    void wipe()
+    {
+        wipe(eeprom_size);
+    }
 
     /**
      * @brief Get the last data stored by the wear-leveling algorithm
      *
      * @return data_t data last stored
      */
-    data_t wl_get_last_data() { return wl_peek(taddr_last).data; }
+    data_t wl_get_last_data()
+    {
+        return wl_peek(taddr_last).data;
+    }
 
     /**
      * @brief read data and pointer stored by the wear-leveling system
@@ -170,13 +182,13 @@ template <class data_t> class WL_AT24CX : public AT24CX {
     wl_data_t<data_t> wl_peek(uint32_t taddr)
     {
         wl_data_t<data_t> out;
-        read(taddr_to_addr(taddr), reinterpret_cast<byte*>(&out), wl_data_size);
+        read(taddr_to_addr(taddr), reinterpret_cast<byte *>(&out), wl_data_size);
         // ESP_LOGD("EEPROM", "Obtained: index %d, addr %d, ptr %d, data %f",
         //  taddr, taddr_to_addr(taddr), out.ptr, out.data);
         return out;
     }
 
-  private:
+   private:
     uint32_t eeprom_size;
 
     uint32_t base_addr;
